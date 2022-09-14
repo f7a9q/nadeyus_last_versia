44716658773cf997c4e599b0efc6afc01699d7c9
f7a9q <856@live.ru>
2022-09-14 22:15:18 +0300

    Add files via upload

--- j.js
+++ j.js
@@ -1,35 +1,42 @@
-async function load_data() {
-    let url = 'https://raw.githubusercontent.com/f7a9q/nadeyus_last_versia/main/routes_output.json';
-    return await (await fetch(url)).json();
+function round(num) {
+    return Math.round(num * 100.0) / 100.0;
 }
-function load(){
-    var mb = document.getElementById("clickme");
-    mb.addEventListener("click", myfunction);
 
-    load_data().then(routes => {
-    console.log('sckdck '+routes.length);
+function load(){
+    //var mb = document.getElementById("clickme");
+    //mb.addEventListener("click", hide_callback);
+    //mb.param = 'displaytable';
+
+    $.getJSON('https://raw.githubusercontent.com/f7a9q/nadeyus_last_versia/main/routes_output.json', function(routes) {
+
+    var hide_callback_var = document.getElementById("clickme");
+    hide_callback_var.addEventListener("click", hide_callback);
+    hide_callback_var.param = 'route' + routes[0]._id;
+    console.log('route' + routes[0]._id);
+
+    let tr = '';
     for (let i = 0; i < routes.length; i++) {
-        let route = routes[i];
-        let tr = '<tr>';
+        var route = routes[i];
+        tr += '<tr>';
         tr += '<td>' + route.name + '</td>';
         tr += '<td>' + route.description + '</td>';
-        tr += '<td>' + route.max_a_percentage + '</td>';
-        tr += '<td>' + route.max_b_percentage + '</td>';
+        tr += '<td>' + (Math.round(route.max_a_percentage * 100.00) / 100.00) + '</td>';
+        tr += '<td>' + (Math.round(route.max_b_percentage * 100.00) / 100.00) + '</td>';
         tr += '</tr>';
         tr += '<tr>';
         tr += '<div id="route' + route._id + '" class="placeholder"><table>';
         for (let j = 0; j < route.route_overlap.length; j++) {
-            let route_overlap = route.route_overlap[j];
+            var route_overlap = route.route_overlap[j];
             tr += '<tr>';
-            tr += '<td>' + route.name + '</td>';
-            tr += '<td>' + route.description + '</td>';
-            tr += '<td>' + route.a_percentage + '</td>';
-            tr += '<td>' + route.b_percentage + '</td>';
+            tr += '<td>' + route_overlap.name + '</td>';
+            tr += '<td>' + route_overlap.description + '</td>';
+            tr += '<td>' + route_overlap.a_percentage + '</td>';
+            tr += '<td>' + route_overlap.b_percentage + '</td>';
             tr += '</tr>';
             tr += '<tr>';
             tr += '<div id="route_overlap' + route._id + '' + route_overlap._id + '" class="placeholder"><table>';
-            for (let k = 0; k < route_overlap.a.length; k++) {
-                let a = route_overlap.a[k];
+            for (let k = 0; false && k < route_overlap.a.length; k++) {
+                var a = route_overlap.a[k];
                 tr += '<tr>' + a.percentage + '</tr>';
                 tr += '<tr>';
                 tr += '<div id="a' + route._id + '' + route_overlap._id + '' + k + '" class="placeholder"><table>';
@@ -44,13 +51,13 @@ function load(){
         }
         tr += '</table></div>';
         tr += '</tr>';
-        painting.querySelector('tbody').innerHTML = tr;
-        //('#painting > tbody tr:last-child').append(tr);
     }
+    console.log(tr);
+    routes_table.querySelector('tbody').innerHTML = tr;
     });
 }
 
-function myfunction() {
-    var tablewrap = document.getElementById('displaytable');
+function hide_callback(evt) {
+    var tablewrap = document.getElementById(evt.currentTarget.param);
     tablewrap.classList.toggle('placeholder')
 };

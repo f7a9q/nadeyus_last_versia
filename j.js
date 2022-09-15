0f9ce6f03413825fca0812fafa796459d00ff98e
f7a9q <856@live.ru>
2022-09-15 09:05:23 +0300

    Add files via upload

--- j.js
+++ j.js
@@ -17,42 +17,52 @@ function load(){
     let tr = '';
     for (let i = 0; i < routes.length; i++) {
         var route = routes[i];
-        tr += '<div class="row">';
+        tr += '<div class="table-row">';
             tr += '<div id="route' + route._id + 'class="table">';
-                tr += '<div class="row">';
-                    tr += '<div class="cell">' + route.name + '</div>'
-                    tr += '<div class="cell">' + route.description + '</div>';
-                    tr += '<div class="cell">' + round(route.max_a_percentage) + '</div>';
-                    tr += '<div class="cell">' + round(route.max_b_percentage) + '</div>';
-                tr += '</div>';
-                tr += '<div class="row">';
-                    tr += '<div id="route_overlap' + route._id + '" class="table">';
-                    for (let j = 0; j < route.route_overlap.length; j++) {
-                        var route_overlap = route.route_overlap[j];
-                        tr += '<div class="table">';
-                            tr += '<div class="row">';
-                                tr += '<div class="cell">' + route_overlap.name + '</div>';
-                                tr += '<div class="cell">' + route_overlap.description + '</div>';
-                                tr += '<div class="cell">' + round(route_overlap.a_percentage) + '</div>';
-                                tr += '<div class="cell">' + round(route_overlap.b_percentage) + '</div>';
-                            tr += '</div>';
-                            tr += '<div class="row">';
-                                tr += '<div id="stops_overlaps' + route._id + '' + route_overlap._id + '" class="placeholder table">';
-                                for (let k = 0; false && k < route_overlap.a.length; k++) {
-                                    var a = route_overlap.a[k];
-                                    tr += '<div class="row">' + a.percentage + '</div>';
-                                    tr += '<div class="row">';
-                                    tr += '<div id="a' + route._id + '' + route_overlap._id + '' + k + '" class="placeholder table">';
-                                    for (let l = 0; l < a.stops.length; l++) {
-                                        tr += '<div class="row">' + a.stops[l].name + '</div>';
-                                    }
+                tr += '<div class="table-body">';
+                    tr += '<div class="table-row">';
+                        tr += '<div class="table-cell">' + route.name + '</div>'
+                        tr += '<div class="table-cell">' + route.description + '</div>';
+                        tr += '<div class="table-cell">' + round(route.max_a_percentage) + '</div>';
+                        tr += '<div class="table-cell">' + round(route.max_b_percentage) + '</div>';
+                    tr += '</div>';
+                    tr += '<div class="table-row">';
+                        tr += '<div id="route_overlap' + route._id + '" class="table">';
+                            tr += '<div class="table-body">';
+                            for (let j = 0; j < route.route_overlap.length; j++) {
+                                var route_overlap = route.route_overlap[j];
+                                tr += '<div class="table">';
+                                    tr += '<div class="table-body">';
+                                        tr += '<div class="table-row">';
+                                            tr += '<div class="table-cell">' + route_overlap.name + '</div>';
+                                            tr += '<div class="table-cell">' + route_overlap.description + '</div>';
+                                            tr += '<div class="table-cell">' + round(route_overlap.a_percentage) + '</div>';
+                                            tr += '<div class="table-cell">' + round(route_overlap.b_percentage) + '</div>';
+                                        tr += '</div>';
+                                        tr += '<div class="table-row">';
+                                            tr += '<div id="stops_overlaps' + route._id + '' + route_overlap._id + '" class="placeholder table">';
+                                                tr += '<div class="table-body">';
+                                                for (let k = 0; false && k < route_overlap.a.length; k++) {
+                                                    var a = route_overlap.a[k];
+                                                    tr += '<div class="table-row">' + a.percentage + '</div>';
+                                                    tr += '<div class="table-row">';
+                                                        tr += '<div id="a' + route._id + '' + route_overlap._id + '' + k + '" class="placeholder table">';
+                                                            tr += '<div class="table-body">';
+                                                            for (let l = 0; l < a.stops.length; l++) {
+                                                                tr += '<div class="table-row">' + a.stops[l].name + '</div>';
+                                                            }
+                                                            tr += '</div>';
+                                                        tr += '</div>';
+                                                    tr += '</div>';
+                                                }
+                                                tr += '</div>';
+                                            tr += '</div>';
+                                        tr += '</div>';
                                     tr += '</div>';
-                                    tr += '</div>';
-                                }
                                 tr += '</div>';
+                            }
                             tr += '</div>';
                         tr += '</div>';
-                    }
                     tr += '</div>';
                 tr += '</div>';
             tr += '</div>';
